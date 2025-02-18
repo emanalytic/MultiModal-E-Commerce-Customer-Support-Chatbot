3a7cbfcf1660128c37c4771a7aad8f31e9a52ad0
Eman <emannisar777@gmail.com>
2025-02-18 20:17:41 +0500

    YOLO commit

--- backend/chatbot.py
+++ backend/chatbot.py
@@ -24,7 +24,6 @@ def get_conversation_chain(text_vectorDB, image_vectorDB):
     - *Query Received*: {input}
     - *Your Database*: {context}
         Use this as your primary knowledge base.
-
     *Guidelines for Usage of Database:*
 
     1. I operate strictly within the bounds of our product database. If an inquiry is about topics outside our catalog, I will suggest alternatives or ask for more details about our products.
@@ -87,4 +86,4 @@ def main(input_dict):
 
 
 input_dict = {'vectorDB_path': 'faiss_index'}
-main(input_dict) 
+main(input_dict)
